dc7245152ab121586e7b399fff8b121421e86e22
plrnvld <paulrijneveld@gmail.com>
2023-08-19 01:32:52 +0200

    Great success!

--- KdTree.java
+++ KdTree.java
@@ -1,45 +1,89 @@
+import java.util.LinkedList;
+
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.SET;
 
 public class KdTree {
+    private SET<Point2D> set;
+
+    private class KdTreeNode {
+        Point2D value;
+        KdTreeNode left;
+        KdTreeNode right;
+
+        KdTreeNode(Point2D value, KdTreeNode left, KdTreeNode right) {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
     public KdTree() { // construct an empty set of points
+        set = new SET<Point2D>();
     }
 
     public boolean isEmpty() {
-        return false;
+        return set.isEmpty();
     }
 
     public int size() { // number of points in the set
-        return -1;
+        return set.size();
     }
 
     public void insert(Point2D p) { // add the point to the set (if it is not already in the set)
         if (p == null)
             throw new IllegalArgumentException();
+
+        var node = new KdTreeNode(p, null, null);
+
+        set.add(node.value);
     }
 
     public boolean contains(Point2D p) { // does the set contain point p?
         if (p == null)
             throw new IllegalArgumentException();
 
-        return false;
+        return set.contains(p);
     }
 
     public void draw() { // draw all points to standard draw
+        for (var point : set) {
+            point.draw();
+        }
     }
 
     public Iterable<Point2D> range(RectHV rect) { // all points that are inside the rectangle (or on the boundary)
         if (rect == null)
             throw new IllegalArgumentException();
 
-        return null;
+        var list = new LinkedList<Point2D>();
+
+        for (var point : set) {
+            if (rect.contains(point)) {
+                list.add(point);
+            }
+        }
+
+        return list;
     }
 
     public Point2D nearest(Point2D p) { // a nearest neighbor in the set to point p; null if the set is empty
         if (p == null)
             throw new IllegalArgumentException();
-            
-        return null;
+
+        var minDist = Double.MAX_VALUE;
+        Point2D champion = null;
+
+        for (var point : set) {
+            var dist = p.distanceTo(point);
+            if (dist < minDist) {
+                minDist = dist;
+                champion = point;
+            }
+        }
+
+        return champion;
     }
 
     public static void main(String[] args) { // unit testing of the methods (optional)
